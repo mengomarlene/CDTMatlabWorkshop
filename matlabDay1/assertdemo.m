dffe894292bde2260f65594a3852c70e69ab91eb
Marlene Mengoni <mengomarlene@gmail.com>
2017-04-20 08:26:15 +0100

    rm --cached .gitignore files

--- matlabDay1/assertdemo.m
+++
@@ -1,7 +0,0 @@
-numbers = [1.5, 2.3, 0.7, -0.001, 4.4];
-total = 0;
-
-for n = numbers
-    assert(n >= 0);
-    total = total + n;
-end
--- matlabDay2/commandLineMatlab/test1.m
+++
@@ -1,6 +0,0 @@
-exchange_rate = 0.5; 
-%amount = input('Give amount in pounds: '); 
-amount=10
-amount_in_foreign_currency = exchange_rate*amount;
-
-disp (amount_in_foreign_currency);
